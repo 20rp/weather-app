53b947edb9456fdac3943c8cb8cef07bef52ae4d
Arlo Wotherspoon <awotherspoon@eit.ac.nz>
2022-05-23 15:42:29 +1200

    Getting response from API.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -11,9 +11,26 @@
 
 // const obj = JSON.parse('{ "location": { "name": "Taradale", "region": "Hawkes Bay", "localtime": "2022-05-20 11:23"}}');
 
-const weatherLocation = '{ "location": {"name": "Taradale", "region": "", "country": "New Zealand", "lat": -39.53, "lon": 176.85, "tz_id": "Pacific/Auckland", "localtime_epoch": 1653002605, "localtime": "2022-05-20 11:23"}}';
-const weatherCurrent = '{ "current": { "last_updated_epoch": 1653002100, "last_updated": "2022-05-20 11:15", "temp_c": 16.8, "temp_f": 62.2, "is_day": 1, "condition": { "text": "Sunny", "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png", "code": 1000}, "wind_mph": 18.8, "wind_kph": 30.2, "wind_degree": 316, "wind_dir": "NW", "pressure_mb": 1002.0, "pressure_in": 29.6, "precip_mm": 0.0, "precip_in": 0.0, "humidity": 56, "cloud": 19, "feelslike_c": 16.8, "feelslike_f": 62.2, "vis_km": 10.0, "vis_miles": 6.0, "uv": 5.0, "gust_mph": 26.4, "gust_kph": 42.5 }}';
+// const weatherLocation = '{ "location": {"name": "Taradale", "region": "", "country": "New Zealand", "lat": -39.53, "lon": 176.85, "tz_id": "Pacific/Auckland", "localtime_epoch": 1653002605, "localtime": "2022-05-20 11:23"}}';
+// const weatherCurrent = '{ "current": { "last_updated_epoch": 1653002100, "last_updated": "2022-05-20 11:15", "temp_c": 16.8, "temp_f": 62.2, "is_day": 1, "condition": { "text": "Sunny", "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png", "code": 1000}, "wind_mph": 18.8, "wind_kph": 30.2, "wind_degree": 316, "wind_dir": "NW", "pressure_mb": 1002.0, "pressure_in": 29.6, "precip_mm": 0.0, "precip_in": 0.0, "humidity": 56, "cloud": 19, "feelslike_c": 16.8, "feelslike_f": 62.2, "vis_km": 10.0, "vis_miles": 6.0, "uv": 5.0, "gust_mph": 26.4, "gust_kph": 42.5 }}';
 
-weatherLocationParsed = JSON.parse(weatherLocation);
-weatherCurrentParsed = JSON.parse(weatherCurrent);
+// weatherLocationParsed = JSON.parse(weatherLocation);
+// weatherCurrentParsed = JSON.parse(weatherCurrent);
 // console.log(weatherCurrentParsed);
+
+
+
+function getData () {
+
+    let result = '';
+
+    const apiUrl = "http://api.weatherapi.com/v1/current.json?key=5068a32470324e3b963231912221905&q=Taradale&aqi=no";
+    const myRequest = new Request(apiUrl);
+    fetch(myRequest).then(res => res.json().then(result => console.log(result)));
+
+    document.getElementById("locationHeader").innerHTML = result.location.name;
+
+    return result;
+}
+getData();
+
