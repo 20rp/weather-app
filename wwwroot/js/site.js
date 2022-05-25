548e65da1b1331757321ad9c5df70ee5b88c174b
Arlo Wotherspoon <awotherspoon@eit.ac.nz>
2022-05-25 14:35:49 +1200

    Removed banner links.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,7 +18,7 @@
         $("#locationHeader").text(result.location.name);
         $("#country").text(result.location.country);
         $("#locationCurrent").text(result.current.condition.text);
-        $("#currentTime").text(result.location.localtime);
+        $("#lastUpdated").text(result.current.last_updated);
 
         $("#windDir").text(result.current.wind_dir);
         $("#currentTemp").text(result.current.temp_c);
@@ -28,71 +28,5 @@
         $("#relHumidity").text(result.current.humidity);
         $("#relHumidity").append("%");
     });
-
-
-
-    // let _res;
-    // let locationName = "Paris";
-    // let airports = ["AKL", "WLG", "CHC", "ZQN", "HLZ"];
-
-    // let apiUrl = "http://api.weatherapi.com/v1/current.json?key=5068a32470324e3b963231912221905&q=" + airports[0] + "&aqi=no";
-
-    // for (index = 0; index < airports.length; index++) {
-    //     let apiUrl = "http://api.weatherapi.com/v1/current.json?key=5068a32470324e3b963231912221905&q=" + airports[index] + "&aqi=no";
-    //     // callApi();
-    //     console.log(apiUrl);
-    // }
-
-    // console.log(apiUrl);
-
-    // function callApi() {
-    //         const weatherCall = async () => {
-    //         // let _res;
-    //         try {
-    //             const res = await fetch(apiUrl);
-    //             _res = await res.json();
-    //         } catch {
-    //             throw new Error("Error, call did not succeed");
-    //         }
-    //         return _res;
-    //     }
-    // }
-
-
-    // $(document).ready(function() {
-        
-    // });
-
-    // $.ajax({})
-    
-
-    // weatherCall().then(res => {
-    //     document.getElementById("locationHeader").innerText = res.location.name;
-    //     document.getElementById("locationHeader").append(", " + res.location.region)
-
-    //     document.getElementById("country").innerText = res.location.country;
-
-    //     document.getElementById("currentTime").innerText = res.location.localtime;
-    //     document.getElementById("currentTime").prepend("The current local time is: ");
-
-
-    //     document.getElementById("weatherImage").src = res.current.condition.icon;
-
-    //     document.getElementById("currentTemp").innerText = res.current.temp_c;
-    //     document.getElementById("currentTemp").append(" Degrees Celcius");
-
-
-    //     document.getElementById("locationCurrent").innerText = res.current.condition.text;
-    //     document.getElementById("windDir").innerText = res.current.wind_dir;
-    //     document.getElementById("windDir").prepend("Wind Direction: ");
-
-    //     document.getElementById("currentWind").innerText = res.current.wind_kph;
-    //     document.getElementById("currentWind").prepend("Wind speed: ");
-    //     document.getElementById("currentWind").append(" K/ph");
-
-    //     document.getElementById("relHumidity").innerText = res.current.humidity;
-    //     document.getElementById("relHumidity").prepend("Relative Humidity: ");
-    //     document.getElementById("relHumidity").append("%");
-    // })
 });
 
